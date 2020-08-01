42f1b246c88e12db98d580253582f410a54a74bd
bourbonracer <bourbonracer@gmail.com>
2020-07-31 22:41:12 -0700

    Adding SchemaTables.sql, EmployeeQueries.sql and gitignore

---
+++ EmployeeSQL/EmployeeQueries.sql
@@ -0,0 +1,74 @@
+select * from departments
+select * from dept_emp
+select * from dept_manager
+select * from employees
+select * from salaries
+select * from titles
+
+-- List the following details of each employee: employee number, last name, first name, sex, and salary.
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees e
+inner join salaries s on e.emp_no = s.emp_no;
+
+-- List first name, last name, and hire date for employees who were hired in 1986.
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1986-01-01' and '1986-12-31';
+
+-- Different way of looking up 1986
+select first_name, last_name, hire_date
+from employees
+where date_part('year', hire_date) = '1986';
+
+-- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+from dept_manager dm
+	inner join employees e
+		on dm.emp_no = e.emp_no
+	inner join departments d
+		on dm.dept_no = d.dept_no;
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+	inner join dept_emp de
+		on e.emp_no = de.emp_no
+	inner join departments d
+		on de.dept_no = d.dept_no;
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+	inner join dept_emp de
+		on e.emp_no = de.emp_no
+	inner join departments d
+		on de.dept_no = d.dept_no
+	where d.dept_name in ('Sales');
+	
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+	inner join dept_emp de
+		on e.emp_no = de.emp_no
+	inner join departments d
+		on de.dept_no = d.dept_no
+	where d.dept_name in ('Sales', 'Development');
+	
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count (last_name) as "Last Name Count"
+from employees
+group by last_name
+order by "Last Name Count" desc;
+
+-- BONUS
+
+-- Epilogue - Ha! Cute!
+
+select first_name, last_name
+from employees e
+where e.emp_no = '499942'
---
+++ EmployeeSQL/SchemaTables.sql
@@ -0,0 +1,65 @@
+-- drop table departments;
+-- drop table dept_emp;
+-- drop table dept_manager;
+-- drop table employees;
+-- drop table salaries;
+-- drop table titles;
+
+create table departments (
+	dept_no varchar,
+	primary key (dept_no),
+	dept_name varchar
+);
+
+select * from departments
+
+create table titles (
+	title_id varchar,
+	primary key(title_id),
+	title varchar
+);
+
+select * from titles
+
+create table employees (
+	emp_no int,
+	primary key(emp_no),
+	emp_title_id varchar,
+	foreign key (emp_title_id) references titles(title_id),
+	birth_date date,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date date
+);
+
+select * from employees
+
+create table dept_emp (
+	emp_no int,
+	foreign key(emp_no) references employees(emp_no),
+	dept_no varchar (20),
+	foreign key(dept_no) references departments(dept_no),
+	primary key(dept_no, emp_no)
+);
+
+select * from dept_emp
+
+create table dept_manager (
+	dept_no varchar,
+	foreign key(dept_no) references departments(dept_no),
+	emp_no int,
+	foreign key(emp_no) references employees(emp_no),
+	primary key(dept_no, emp_no)
+);
+
+select * from dept_manager
+
+create table salaries (
+	emp_no int,
+	foreign key(emp_no) references employees(emp_no),
+	salary varchar,
+	primary key(emp_no, salary)
+);
+
+select * from salaries
